e4199f175b307c1e3b7eeb0bb0b963ee7b7f67b6
PodPat <80781711+PodPat@users.noreply.github.com>
2025-01-10 18:30:38 +0100

    [fix]

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from datetime import datetime
 
 app = Flask(__name__)
-@app.route('/hello/<name>')
-def hello_name(name):
-    return 'Hello, %s!' % name
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
